373fb4279e4a0cee2446c79d1e9223800c1cf9bd
Sinai <sina.m.igneel01@gmail.com>
2024-11-19 14:13:36 -0600

    cambiado el puerto a 3000

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import providerRoutes from './routes/providers.routes.js'
 
 export const startServer = () => {
     const app = express();
-    const port = process.env.PORT || 3002
+    const port = process.env.PORT || 3000
 
     app.use(express.json())
     app.use('/api', providerRoutes);
