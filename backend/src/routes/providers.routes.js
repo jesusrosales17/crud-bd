5e8a23bae5b63244a078cb16f91533505a8388c5
Jesus Rosales Castillo <jesusrosales07537@gmail.com>
2024-11-19 12:40:00 -0600

    primer commit

---
+++ backend/src/routes/providers.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import {  getAllProviders } from "../controllers/providers.controller.js";
+
+const router = Router();
+
+router.get('/providers', getAllProviders);
+
+
+export default router;
---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import providerRoutes from './routes/providers.routes.js'
+
+export const startServer = () => {
+    const app = express();
+    const port = process.env.PORT || 3002
+
+    app.use(express.json())
+    app.use('/api', providerRoutes);
+
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    })
+}
