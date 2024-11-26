283f160b08bdaa78a4754ef5c855e634c9bd4410
Sinai <sina.m.igneel01@gmail.com>
2024-11-26 10:50:41 -0600

    ningun cambio

--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -3,10 +3,6 @@ import { deleteProducts } from "../controllers/products.controller.js";
 
 const router = Router();
 
-router.get('/products', (req, res) => {
-    res.send('products');
-});
-
 router.delete("/delete",deleteProducts)
 
 export default router;
