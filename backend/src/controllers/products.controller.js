55f793f15401dcfae7624a79695d7388c30eb7d3
Enrique <josetayrong@gmail.com>
2024-12-03 15:11:09 -0600

    nose

--- backend/src/controllers/products.controller.js
+++ backend/src/controllers/products.controller.js
@@ -14,7 +14,7 @@ export const addProduct = async (req, res) => {
 
 
     
-
+    
     
     
 }
--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -1,12 +1,8 @@
 import {Router} from "express"; 
-import { addProduct, deleteProducts } from "../controllers/products.controller.js";
-import {getProducts,  deleteProducts } from "../controllers/products.controller.js";
+import {getProducts,  deleteProducts, addProduct } from "../controllers/products.controller.js";
 
 const router = Router();
 
-router.get('/products', (req, res) => {
-    res.send('products');
-});
 
 router.delete("/products",deleteProducts)
 router.post("/products",addProduct)
